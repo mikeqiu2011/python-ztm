3bd4f780c66d48d8a4bc59edeca6e4f50fefaf99
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-25 11:26:54 +0800

    for loop

---
+++ flow/for_loop.py
@@ -0,0 +1,18 @@
+li = list(range(100))
+
+# for item in li:
+#     print(item)
+#
+# for char in 'hi there, good day!!':
+#     print(char)
+
+for item in range(100):
+    print(item)
+
+for item in (1,2,3,4,5):
+    print(item)
+
+for item in {1,2,3,4,5}:
+    print(item)
+
+print(item) # 5, item can be accessed outside of block scope...
