4f8a6e5f88a42b52b01657908bbeee43065fc76c
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-25 14:25:33 +0800

    my solution

---
+++ flow/exe-gui.py
@@ -0,0 +1,31 @@
+# Exercise!
+# Display the image below to the right hand side where the 0 is going to be ' ', and the 1 is going to be '*'. This will reveal an image!
+picture = [
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0]
+]
+
+
+# print(len(picture))
+# print(len(picture[0]))
+
+def display_pic(picture):
+    for i in range(len(picture)):
+        for j in range(len(picture[i])):
+            if picture[i][j] == 0:
+                print(' ', end=' ')
+            else:
+                print('*', end=' ')
+        print()
+
+#       *
+#     * * *
+#   * * * * *
+# * * * * * * *
+#       *
+#       *
+display_pic(picture)
