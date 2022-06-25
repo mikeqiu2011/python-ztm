7bd46dea25be3a4bb672002c6bb513de35e5cf35
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-25 20:58:25 +0800

    scope rules

--- flow/scope.py
+++ flow/scope.py
@@ -1,21 +1,37 @@
-# Scope - what variables do i have access to
+# # Scope - what variables do i have access to
+#
+# total = 100 # global scope
+#
+# print(total)
+#
+# def some_func():
+#     total2 = 10
+#     print(total) # function can access global variable
+#
+# #print(total2) # cannot access function scope
+#
+# some_func()
+#
+#
+# # block scope
+#
+# if True:
+#     x = 10
+#
+# print(x) # 10, can access block scope
 
-total = 100 # global scope
-
-print(total)
-
-def some_func():
-    total2 = 10
-    print(total) # function can access global variable
-
-#print(total2) # cannot access function scope
-
-some_func()
+# Scope rule:
+#1 start with local(e.g. create a same name variable if has assignment)
+#2 - Parent local?
+#3 - global
+#4 - built-in python functions
 
 
-# block scope
+a = 1
+def confusion():
+    a = 5   # the function creates a new local variable a
+    return a
 
-if True:
-    x = 10
-
-print(x) # 10, can access block scope
+print(a) # 1
+print(confusion()) # 5
+print(a) # 1
