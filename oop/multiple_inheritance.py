7cafc2f00ec01b2a696406eadbef2433502cd447
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-26 15:43:54 +0800

    multiple-inheritance not working

---
+++ oop/multiple_inheritance.py
@@ -0,0 +1,41 @@
+class User():
+    def __init__(self, name) -> None:
+        self._name = name
+
+    def sign_in(self):
+        print('logged in')
+
+    def attack(self):
+        print('do nothing')
+
+
+class Wizard(User):
+    def __init__(self, name, power) -> None:
+        super().__init__(name)
+        self._power = power
+
+    def attack(self):
+        # User.attack(self)
+        super().attack()
+        print(f'attack with power {self._power}')
+
+
+class Archer(User):
+    def __init__(self, name, num_arrows) -> None:
+        super().__init__(name)
+        self._num_arrows = num_arrows
+
+    def check_arrows(self):
+        print(f'{self._num_arrows} arrows left!')
+
+    def run(self):
+        print('ran very fast')
+
+
+class HybridBorg(Wizard, Archer):
+    pass
+
+
+hb = HybridBorg('simon', 20)
+hb.attack()
+# hb.check_arrows()
