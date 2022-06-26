e88b349fe2c12ac2a8b41ac2e70d575d6cb8a781
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-26 11:12:29 +0800

    shout myname

--- oop/class.py
+++ oop/class.py
@@ -2,10 +2,11 @@ class PlayerCharacter:
     # Class Attribute
     membership = True # static
     def __init__(self, name) -> None:  # constructor method
-        self.name = name # property
+        if self.membership:
+            self.name = name # property
 
-    def run(self):
-        print('run')
+    def shout(self):
+        print(f'my name is {self.name}')
 
 p1 = PlayerCharacter('mike') # when run (), the __init__ is invoked
 p2 = PlayerCharacter('kevin')
@@ -13,9 +14,12 @@ p2 = PlayerCharacter('kevin')
 print(p1.name)
 print(p2.name)
 
-p1.run()
+p1.shout()
 
 p2.attack = 50
 print(p2.attack)  # 50
 
 print(p1.membership) # True
+p1.membership = False
+print(p1.membership) # changed to False
+print(p2.membership) # still True
