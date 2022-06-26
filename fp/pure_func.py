c9eaa5b75a3654ac4b0d4c163f8fb8edede750ce
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-26 16:23:34 +0800

    now it is pure

--- fp/pure_func.py
+++ fp/pure_func.py
@@ -1,7 +1,5 @@
-new_list = []
-
-
 def multiply_by2(li):
+    new_list = []  # will not affect the outside world
     for item in li:
         new_list.append(item)
 
@@ -9,5 +7,7 @@ def multiply_by2(li):
 
 
 my_list = [1, 2, 3]
+
+# same result no matter how many times you run
 print(multiply_by2(my_list))  # [1, 2, 3]
-print(multiply_by2(my_list))  # [1, 2, 3, 1, 2, 3]
+print(multiply_by2(my_list))  # [1, 2, 3]
