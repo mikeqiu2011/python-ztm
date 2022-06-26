6aac808e7272d13cbf6a7e1f357f36fc2a961529
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-26 16:49:22 +0800

    using lambda

--- fp/filter.py
+++ fp/filter.py
@@ -5,6 +5,7 @@ def is_even(num):
     return num % 2 == 0
 
 
-list2 = list(filter(is_even, list1))
+# map always return the same number of item of original one, while filter normally return less than the origin
+list2 = list(filter(lambda x: x % 2 == 0, list1))
 
 print(list2)  # [0, 2, 4, 6, 8]
