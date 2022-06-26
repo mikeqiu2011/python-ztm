84472e6833c7167afe7cb8e91471dcd14cb9baaf
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-26 16:42:25 +0800

    now map works

--- fp/map.py
+++ fp/map.py
@@ -1,12 +1,11 @@
-list1 = range(10)
+list1 = list(range(10))
 
 
-def multiply_by2(li):
-    new_list = []
-    for item in li:
-        new_list.append(item)
-    return new_list
+def multiply_by2(x):
+    return x*2
 
 
+# map will take each item in the iterable to be processed by the func and return
 list2 = list(map(multiply_by2, list1))
 print(list2)
+print(list1)  # no change to original list
