2c028a0124946f65038df9857f3de80db9d34b65
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-26 14:47:03 +0800

    polymorphism

--- oop/inheritance.py
+++ oop/inheritance.py
@@ -5,6 +5,9 @@ class User():
     def sign_in(self):
         print('logged in')
 
+    def attack(self):
+        print('do nothing')
+
 
 class Wizard(User):
     def __init__(self, name, power) -> None:
@@ -28,10 +31,10 @@ class Archer(User):
 wizard1 = Wizard('gandof', 10)
 archer1 = Archer('Robin', 30)
 
-print(isinstance(wizard1, Wizard))  # True
-print(isinstance(wizard1, User))   # True
-print(isinstance(wizard1, object))   # True
-print(isinstance(wizard1, Archer))  # False
+# print(isinstance(wizard1, Wizard))  # True
+# print(isinstance(wizard1, User))   # True
+# print(isinstance(wizard1, object))   # True
+# print(isinstance(wizard1, Archer))  # False
 
 # wizard1.sign_in()
 # wizard1.attack()
@@ -39,3 +42,16 @@ print(isinstance(wizard1, Archer))  # False
 # archer1.sign_in()
 # archer1.attack()
 # archer1.attack()
+
+# Polymorphism
+
+
+def player_attack(char):
+    char.attack()
+
+
+player_attack(wizard1)
+player_attack(archer1)
+
+for char in [wizard1, archer1]:
+    char.attack()
