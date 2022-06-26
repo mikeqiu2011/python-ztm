a09e9bfaed7a1554565aec2a253e439fb8050c3b
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-26 11:45:58 +0800

    @classmethod

--- oop/class.py
+++ oop/class.py
@@ -1,17 +1,19 @@
 class PlayerCharacter:
     # Class Attribute
-    membership = True # static, not change for different obj
+    membership = True  # static, not change for different obj
+
     def __init__(self, name='anonymous', age=0) -> None:  # constructor method
-        if age > 18:
-            self.name = name # object property, dynamic for different obj
-            self.age = age
+        self.name = name  # object property, dynamic for different obj
+        self.age = age
 
     def shout(self):
         print(f'my name is {self.name}')
 
-p1 = PlayerCharacter('tom', 10) # when run (), the __init__ is invoked
+    @classmethod
+    def adding_things(num1, num2):
+        return num1 + num2
 
-print(p1.name) # object has no attribute 'name'
 
-
-
+p1 = PlayerCharacter('tom', 10)  # when run (), the __init__ is invoked
+# adding_things() takes 2 positional arguments but 3 were given
+print(p1.adding_things(2, 3))
