47dc8da8e097231175260a8ba342e7c0f647152b
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-26 22:22:18 +0800

    dictionary comprehension

--- fp/set-dict-comprehension.py
+++ fp/set-dict-comprehension.py
@@ -1,7 +1,21 @@
 # set comprehension
 
-my_list = {char for char in 'hello'}
-print(my_list)
+# my_list = {char for char in 'hello'}
+# print(my_list)
 
-my_list2 = {num for num in range(10)}
-print(my_list2)
+# my_list2 = {num for num in range(10)}
+# print(my_list2)
+
+# dictionary comprehension
+
+simple_dict = {
+    'a': 1,
+    'b': 2
+}
+
+my_dict = {k+'i': v**2 for k,
+           v in simple_dict.items() if v % 2 == 0}
+print(my_dict)  # {'bi': 4}
+
+my_dict2 = {num: num*2 for num in [1, 2, 3]}
+print(my_dict2)
