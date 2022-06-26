b16874b792d8962680bee30860a110caecbb3697
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-26 16:55:58 +0800

    tuple or set are all fine

--- fp/zip.py
+++ fp/zip.py
@@ -1,5 +1,6 @@
-my_list = [1, 2, 3, 4]
-your_list = [10, 20, 30]
+# ok if one of the iterable is a tuple,as long as it is iterable
+my_list = (1, 2, 3, 4)
+your_list = {10, 20, 30}  # set is also fine
 his_list = ['a', 'b', ]  # result will be based on the shortest one
 
 # zip will zip different items together as individual tuple
