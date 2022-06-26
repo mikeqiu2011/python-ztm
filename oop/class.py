8ffe86153e4fe2b2709b58d1afa71b660543efc9
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-26 11:52:20 +0800

    @staticmethod

--- oop/class.py
+++ oop/class.py
@@ -13,10 +13,15 @@ class PlayerCharacter:
     def adding_things(cls, num1, num2):
         return cls('Teddy', num1 + num2)
 
+    @staticmethod  # you do not have access to cls in static method
+    def adding_things2(num1, num2):
+        return num1 + num2
 
+
+print(PlayerCharacter.adding_things2(2, 3))  # 5
 # class method does not require instance
-player3 = PlayerCharacter.adding_things(3, 4)
-print(player3.age)
+# player3 = PlayerCharacter.adding_things(3, 4)
+# print(player3.age)
 # p1 = PlayerCharacter('tom', 10)  # when run (), the __init__ is invoked
 # # adding_things() takes 2 positional arguments but 3 were given
 # print(p1.adding_things(2, 3))
