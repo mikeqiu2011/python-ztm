6157699e54dc05a48d6e10946b4e682052b68bef
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-27 17:37:18 +0800

    simplest yield

--- fp/generator.py
+++ fp/generator.py
@@ -1,17 +1,10 @@
-# list is in memory, it takes the whole space
-
-def make_list(num):
-    result = []
-
-    for i in range(num):
-        result.append(i*2)
-
-    return result
-
-
-my_list = make_list(100)
-print(my_list)
-
-
 # range() is a generator, which gives you one by one
 # if you need a huge list, the list must be first created in memory as a whole before you can use it, which is not efficient
+
+def generator_function(num):
+    for i in range(num):
+        yield i  # instead returning, generator yield
+
+
+for item in generator_function(1000):
+    print(item)
