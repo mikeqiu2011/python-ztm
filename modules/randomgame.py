085b3b532da6f3ce4968823643dcec14f8cfaa07
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-27 22:16:11 +0800

    my solution

---
+++ modules/randomgame.py
@@ -0,0 +1,23 @@
+import sys
+import random
+
+min = int(sys.argv[1])
+max = int(sys.argv[2])
+
+# print(min, max)
+
+target = random.randint(min, max)
+# print(target)
+count = 0
+while True:
+    answer = int(input(f'guess a number between {min} and {max} pls \n'))
+    if answer == target:
+        print('correct!')
+        break
+    elif answer > target:
+        print('too big!')
+    else:
+        print('too small')
+
+    count += 1
+    print(f'you guessed {count} times')
