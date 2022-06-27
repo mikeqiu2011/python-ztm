29d0e56b6500fc298beeca90286b161c2e208ecf
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-27 21:24:12 +0800

    if __name__ == '__main__':

--- modules/main.py
+++ modules/main.py
@@ -1,10 +1,17 @@
 from audioop import mul
 # do not import *, be explicity to avoid name collision
-from utility import sum, multiply, max
+from utility import sum, multiply
 from shopping.more_shopping import shopping_cart
 
 print(multiply(2, 4))
 
 print(shopping_cart.buy('apple'))
 
-# print(max([1, 2, 3])) # name collision
+
+# '__main__' is given to the specific file that we run
+print(__name__)
+
+
+if __name__ == '__main__':
+    # only run file when this is the main file instead of a module
+    print('pls run this')
--- modules/utility.py
+++ modules/utility.py
@@ -1,3 +1,6 @@
+print(__name__)
+
+
 def sum(num1, num2):
     return num1 + num2
 
@@ -8,3 +11,7 @@ def multiply(num1, num2):
 
 def max():
     return 'oops'
+
+
+if __name__ == '__main__':
+    print('pls run this')   # will not be run
