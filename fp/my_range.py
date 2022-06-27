5cba7a7468e5f4b40ef9780255ec9629ca7f63cd
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-27 18:16:33 +0800

    my_range implementation

---
+++ fp/my_range.py
@@ -0,0 +1,20 @@
+class MyRange:
+    def __init__(self, first, last) -> None:
+        self.current = 0
+        self.first = first
+        self.last = last
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current < self.last:
+            num = self.current
+            self.current += 1
+            return num
+
+        raise StopIteration  # there is no item left, stop iteration
+
+
+for i in MyRange(0, 100):
+    print(i)
