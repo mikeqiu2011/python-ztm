392eac381c180b8c61d821772a0a599d32a4c952
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-27 17:09:07 +0800

    finally

--- advanced/error-handling.py
+++ advanced/error-handling.py
@@ -1,28 +1,31 @@
 # non handled errors will crash our program
 
 
-# while True:
-#     try:
-#         age = int(input('enter your age:\n'))
-#         10/age
-
-#     except ValueError:
-#         print('Pls enter a number')
-#     except ZeroDivisionError:
-#         print('zero is not allowed')
-#     else:
-#         print('thank you')
-#         break
-
-
-def sum(num1, num2):
+while True:
     try:
-        return num1/num2
-    except (TypeError, ZeroDivisionError) as err:
-        print(err)
+        age = int(input('enter your age:\n'))
+        10/age
+
+    except ValueError:
+        print('Pls enter a number')
+        print(age)
+    except ZeroDivisionError:
+        print('zero is not allowed')
+    else:
+        print('thank you')
+        break
+    finally:
+        print(f'user entered {age}! activity logged!')
 
 
-result = sum('1', 2)  # error caught, None
-print(result)
+# def sum(num1, num2):
+#     try:
+#         return num1/num2
+#     except (TypeError, ZeroDivisionError) as err:
+#         print(err)
 
-sum(1, 0)
+
+# result = sum('1', 2)  # error caught, None
+# print(result)
+
+# sum(1, 0)
