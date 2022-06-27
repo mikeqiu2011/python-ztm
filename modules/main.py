09849700755af7f35dfa5e82b20462c8117fe168
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-27 21:05:40 +0800

    package inside package

--- modules/main.py
+++ modules/main.py
@@ -1,7 +1,6 @@
 import utility
-import shopping.shopping_cart
-
+import shopping.more_shopping.shopping_cart
 # <module 'utility' from '/Users/mikeqiu/code/python/python-ztm/modules/utility.py'>
 print(utility.multiply(2, 4))
 
-print(shopping.shopping_cart.buy('apple'))
+print(shopping.more_shopping.shopping_cart.buy('apple'))
