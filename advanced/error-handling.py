5a3366d2be93592f1bbaed1c1f38c55f3aab86c1
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-27 16:56:15 +0800

    During handling of the above exception, another exception occurred:

--- advanced/error-handling.py
+++ advanced/error-handling.py
@@ -19,7 +19,7 @@ def sum(num1, num2):
     try:
         return num1 + num2
     except TypeError as err:
-        print('pls enter numbers', err)
+        print('pls enter numbers' + err)
 
 
 result = sum('1', 2)  # error caught, None
