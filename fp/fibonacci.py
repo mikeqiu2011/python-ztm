abd520b4315bf6f33e28fd133113babed5ca76a5
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-27 18:31:08 +0800

    my solution for FibGen

---
+++ fp/fibonacci.py
@@ -0,0 +1,24 @@
+class FibGen:
+    def __init__(self, num):
+        self.cur_index = 0
+        self.num = num
+        self.x = 0
+        self.y = 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.cur_index < self.num:
+            result = self.x + self.y
+            self.x = self.y
+            self.y = result
+
+            self.cur_index += 1
+            return result
+
+        raise StopIteration
+
+
+for i in FibGen(20):
+    print(i)
