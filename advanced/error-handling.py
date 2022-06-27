d66cd0bb5961b5218adfec55f4c15f79622b661f
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-27 16:49:45 +0800

    sum with flaw

--- advanced/error-handling.py
+++ advanced/error-handling.py
@@ -1,15 +1,23 @@
 # non handled errors will crash our program
 
 
-while True:
-    try:
-        age = int(input('enter your age:\n'))
-        10/age
+# while True:
+#     try:
+#         age = int(input('enter your age:\n'))
+#         10/age
 
-    except ValueError:
-        print('Pls enter a number')
-    except ZeroDivisionError:
-        print('zero is not allowed')
-    else:
-        print('thank you')
-        break
+#     except ValueError:
+#         print('Pls enter a number')
+#     except ZeroDivisionError:
+#         print('zero is not allowed')
+#     else:
+#         print('thank you')
+#         break
+
+
+def sum(num1, num2):
+    return num1 + num2
+
+
+result = sum('1', '2')  # '12' !!
+print(result)
