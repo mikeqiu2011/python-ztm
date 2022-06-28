d2115d33c0e7f9b844826bd5e09a1f41cd2ce1d9
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-28 17:34:45 +0800

    match obj properties

--- regex/regex-test.py
+++ regex/regex-test.py
@@ -1,8 +1,9 @@
 import re
 
-string = 'search inside of this text please'
+string = 'search this inside of this text please'
 
-print('search' in string)  # True
-
-print(re.search('this', string))  # returns a Match object, occurs at the
-# index of 17 and ends at 21 - <re.Match object; span=(17, 21), match='this'>
+a = re.search('this', string)
+print(a.span())  # (17, 21)
+print(a.start())  # 17
+print(a.end())  # 21
+print(a.group())  # this
