b373c8d6cb7d9a047602c2b860b284ec0b177671
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-28 20:43:02 +0800

    unittest.main()

--- test/main.py
+++ test/main.py
@@ -1,5 +1,7 @@
-def do_stuff(num):
+def do_stuff(num=0):
     try:
+        if not num:  # if receive None or '', convert to 0
+            num = 0
         return int(num) + 5
     except ValueError as err:
         return err
--- test/test.py
+++ test/test.py
@@ -3,14 +3,30 @@ from main import do_stuff
 
 
 class TestMain(unittest.TestCase):
-    def test_do_stuff(self):
+    def test_do_stuff_input_normal(self):
         test_param = 10
         result = do_stuff(test_param)
         self.assertEqual(result, 15)
 
-    def test_do_stuff2(self):
+    def test_do_stuff_input_string(self):
         test_param = 'sasas'
         result = do_stuff(test_param)
         self.assertIsInstance(result, ValueError)
 
-# unittest.main()
+    def test_do_stuff_input_none(self):
+        test_param = None
+        result = do_stuff(test_param)
+        self.assertEqual(result, 5)
+
+    def test_do_stuff_input_empty_string(self):
+        test_param = ''
+        result = do_stuff(test_param)
+        self.assertEqual(result, 5)
+
+    def test_do_stuff_input_default(self):
+        result = do_stuff()
+        self.assertEqual(result, 5)
+
+
+if __name__ == '__main__':
+    unittest.main()
