d0c7e2e8b2e0df47aecce60278ec32ee1a57ee86
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-28 10:36:49 +0800

    Counter

---
+++ modules/fav-module.py
@@ -0,0 +1,8 @@
+from collections import Counter, defaultdict, OrderedDict
+
+# count duplicated items, to be optimized
+li = [1,2,4,5,6,7,7,7,8,6,2]
+print(Counter(li)) #
+
+sentence = 'hi hi hi python is good'
+print(Counter(sentence)) # Counter({' ': 5, 'h': 4, 'i': 4, 'o': 3, 'p': 1, 'y': 1, 't': 1, 'n': 1, 's': 1, 'g': 1, 'd': 1})
--- modules/randomgame.py
+++ modules/randomgame.py
@@ -10,14 +10,17 @@ target = random.randint(min, max)
 # print(target)
 count = 0
 while True:
-    answer = int(input(f'guess a number between {min} and {max} pls \n'))
-    if answer == target:
-        print('correct!')
-        break
-    elif answer > target:
-        print('too big!')
-    else:
-        print('too small')
+    try:
+        answer = int(input(f'guess a number between {min} and {max} pls \n'))
+        if answer == target:
+            print('correct!')
+            break
+        elif answer > target:
+            print('too big!')
+        else:
+            print('too small')
 
-    count += 1
-    print(f'you guessed {count} times')
+        count += 1
+        print(f'you guessed {count} times')
+    except ValueError:
+        print('must be a number')
