70570ebbf6a3912ea0f6f67f77cd85ad91335531
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-28 16:00:35 +0800

    file.seek()

--- io/file.py
+++ io/file.py
@@ -1,5 +1,9 @@
 my_file = open('test.txt')
 print(my_file)  # <_io.TextIOWrapper name='test.txt' mode='r' encoding='UTF-8'>
 print(my_file.read())  # content in test.txt
+
+my_file.seek(0)  # restart the cursor from offset 0
 print(my_file.read())  # blank
-print(my_file.read())  # blank, but why?
+
+my_file.seek(4)  #
+print(my_file.read())  # my name is mike
