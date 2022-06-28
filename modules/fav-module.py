35e6a117f7711c6adc400f77ddff9dc97244155f
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-28 10:47:09 +0800

    OrderedDict - the order of adding item matters

--- modules/fav-module.py
+++ modules/fav-module.py
@@ -8,9 +8,29 @@ from collections import Counter, defaultdict, OrderedDict
 # print(Counter(sentence)) # Counter({' ': 5, 'h': 4, 'i': 4, 'o': 3, 'p': 1, 'y': 1, 't': 1, 'n': 1, 's': 1, 'g': 1, 'd': 1})
 
 
-my_dict = defaultdict(lambda: 5, {
-    'a': 1,
-    'b': 2,
-})
-print(my_dict['a'])
-print(my_dict['c']) # now instead of keyError, it will return default value of 5
+# my_dict = defaultdict(lambda: 5, {
+#     'a': 1,
+#     'b': 2,
+# })
+# print(my_dict['a'])
+# print(my_dict['c']) # now instead of keyError, it will return default value of 5
+
+d = {}
+d['a'] = 1
+d['b'] = 2
+
+d2 = {}
+d2['b'] = 2
+d2['a'] = 1
+
+print(d2 == d)  # True
+
+d3 = OrderedDict()
+d3['a'] = 1
+d3['b'] = 2
+
+d4 = OrderedDict()
+d4['b'] = 2
+d4['a'] = 1
+
+print(d4 == d3)  # False
