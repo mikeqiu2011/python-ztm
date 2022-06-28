df1395324484d9f060815fa9c6292e788c833b1e
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-28 16:13:54 +0800

    read and write

--- io/file.py
+++ io/file.py
@@ -1,3 +1,5 @@
 # using with, no need to manually close the file after read
-with open('test.txt', mode='r') as f:
+with open('test.txt', mode='r+') as f:
     print(f.readlines())
+    text = f.write('hi, it is me!!')
+    print(text)
