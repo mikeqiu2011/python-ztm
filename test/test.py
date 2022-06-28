865a81a7a288728635cd5a2ae692c11764401718
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-28 20:14:06 +0800

    self.assertTrue(isinstance(result, ValueError))

--- test/test.py
+++ test/test.py
@@ -3,10 +3,14 @@ from main import do_stuff
 
 
 class TestMain(unittest.TestCase):
-    def test_do_studff(self):
-        num = 10
-        result = do_stuff(num)
-        self.assertEqual(15, result)
+    def test_do_stuff(self):
+        test_param = 10
+        result = do_stuff(test_param)
+        self.assertEqual(result, 15)
 
+    def test_do_stuff2(self):
+        test_param = 'sasas'
+        result = do_stuff(test_param)
+        self.assertTrue(isinstance(result, ValueError))
 
-unittest.main()
+# unittest.main()
