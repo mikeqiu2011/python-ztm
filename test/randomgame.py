1a53951f9ee5afd15dc4cdbeaf881041b496151f
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-28 21:10:45 +0800

    first extract the core logic into function to be tested later

---
+++ test/randomgame.py
@@ -0,0 +1,36 @@
+import sys
+import random
+
+min = int(sys.argv[1])
+max = int(sys.argv[2])
+
+
+def check_answer(answer, target):
+    if answer == target:
+        print('correct!')
+        return True
+    elif answer > target:
+        print('too big!')
+    else:
+        print('too small')
+
+
+def guess_a_num(min, max):
+    target = random.randint(min, max)
+    count = 0
+    while True:
+        try:
+            answer = int(
+                input(f'guess a number between {min} and {max} pls \n'))
+
+            if check_answer(answer, target):
+                break
+
+            count += 1
+            print(f'you guessed {count} times')
+        except ValueError:
+            print('must be a number')
+
+
+if __name__ == '__main__':
+    guess_a_num(min, max)
