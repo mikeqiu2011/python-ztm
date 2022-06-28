0c2b3524533413f2884bc145e1c5e7e6a465628a
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-28 17:43:33 +0800

    findall, fullmatch, match

--- regex/regex-test.py
+++ regex/regex-test.py
@@ -9,3 +9,10 @@ print(result.group())  # this
 
 b = pattern.findall(string)
 print(b)  # ['this', 'this']
+
+c = pattern.fullmatch(string)
+print(c)  # None, the pattern shall be full match to the entire string
+
+pattern = re.compile('search this')
+d = pattern.match(string)  # the pattern need to match from the start
+print(d)
