e48cdef3a71297cad57133773cc3f3633bb861e1
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-28 17:35:34 +0800

    no matching

--- regex/regex-test.py
+++ regex/regex-test.py
@@ -2,8 +2,5 @@ import re
 
 string = 'search this inside of this text please'
 
-a = re.search('this', string)
-print(a.span())  # (17, 21)
-print(a.start())  # 17
-print(a.end())  # 21
-print(a.group())  # this
+a = re.search('thIs', string)  # now a is None
+print(a.group())  # will cause error
