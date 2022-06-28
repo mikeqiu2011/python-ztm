8747a9176861a34c5b054cbda99e541bb96453fb
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-28 16:10:10 +0800

    with open

--- io/file.py
+++ io/file.py
@@ -1,5 +1,3 @@
-my_file = open('test.txt')
-print(my_file)  # <_io.TextIOWrapper name='test.txt' mode='r' encoding='UTF-8'>
-print(my_file.readlines())  # ['hi, my name is mike\n', ':)\n', 'how are you']
-
-my_file.close()
+# using with, no need to manually close the file after read
+with open('test.txt', mode='r') as f:
+    print(f.readlines())
