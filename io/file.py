edec4e630e2a6779798dd832d3ce4061f07690da
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-28 16:02:20 +0800

    my_file.readline()

--- io/file.py
+++ io/file.py
@@ -1,9 +1,4 @@
 my_file = open('test.txt')
 print(my_file)  # <_io.TextIOWrapper name='test.txt' mode='r' encoding='UTF-8'>
-print(my_file.read())  # content in test.txt
-
-my_file.seek(0)  # restart the cursor from offset 0
-print(my_file.read())  # blank
-
-my_file.seek(4)  #
-print(my_file.read())  # my name is mike
+print(my_file.readline())
+print(my_file.readline())
