e3a770fa58ea5d4dffec9fd6ffcef331781c210e
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-28 16:08:15 +0800

    close file after read

--- io/file.py
+++ io/file.py
@@ -1,3 +1,5 @@
 my_file = open('test.txt')
 print(my_file)  # <_io.TextIOWrapper name='test.txt' mode='r' encoding='UTF-8'>
 print(my_file.readlines())  # ['hi, my name is mike\n', ':)\n', 'how are you']
+
+my_file.close()
