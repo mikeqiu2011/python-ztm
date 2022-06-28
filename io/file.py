1c78696e741596631a4a8c85cda7c769ac190711
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-28 16:25:27 +0800

    relative path on unix

--- io/file.py
+++ io/file.py
@@ -1,4 +1,4 @@
 # using with, no need to manually close the file after read
-with open('sad.txt', mode='w') as f:
+with open('app/sad.txt', mode='w') as f:
     text = f.write(':(')
     print(text)
---
+++ io/path.py
@@ -0,0 +1,3 @@
+# below works fine on unix system, but not working on windows
+with open('app/sad.txt') as f:
+    print(f.readlines())
