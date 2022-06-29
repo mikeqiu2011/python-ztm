56ede3025edded9a933d99ed55ed39f7eac92f55
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-29 10:27:45 +0800

    using html and template for email body

--- email/email_sender.py
+++ email/email_sender.py
@@ -1,6 +1,8 @@
 import smtplib
 from email.message import EmailMessage
 import os
+from string import Template
+from pathlib import Path
 
 username = os.getenv('USER')
 password = os.getenv('PASSWORD')
@@ -10,7 +12,8 @@ email['from'] = 'mike qiu'
 email['to'] = 'mikeqiu2011@163.com'
 email['subject'] = 'how are you'
 
-email.set_content('i am a Python master!')
+html = Template(Path('index.html').read_text())
+email.set_content(html.substitute({'name': 'tintin'}), 'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
