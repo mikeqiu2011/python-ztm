e8ce5b2a79d8c005e5d50cf9c2d3d68214081b0a
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-29 20:55:12 +0800

    blur image

--- images/images.py
+++ images/images.py
@@ -1,7 +1,10 @@
-from PIL import Image
+from PIL import Image, ImageFilter
 
 img = Image.open('./pokedex/pikachu.jpg')
 
 print(img.format)  # JPEG
 print(img.size)  # (640,640)
 print(img.mode)  # RGB
+
+filtered_img = img.filter(ImageFilter.BLUR)
+filtered_img.save('blur.png')
