27238dd0b0fa9fb3bfb3ed605990bf0c7c6bab70
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-29 21:41:55 +0800

    tutor solution

--- images/JPGtoPNGconverter.py
+++ images/JPGtoPNGconverter.py
@@ -6,11 +6,13 @@ folder_in = sys.argv[1]
 folder_out = sys.argv[2]
 
 print(folder_in)
-# os.makedirs(folder_out)
+if not os.path.exists(folder_out):
+    os.makedirs(folder_out)
 
 for file in os.listdir(folder_in):
     print(file)
-    filename, extension = file.split('.')
-    if extension == 'jpg':
+    # filename, extension = file.split('.')
+    [filename, extension] = os.path.splitext(file)
+    if extension.lower() in ['.jpg', '.jpeg']:
         new_file = Image.open(f'{folder_in}/{file}')
         new_file.save(f'{folder_out}/{filename}.png')
