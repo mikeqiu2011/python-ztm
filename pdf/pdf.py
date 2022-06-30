9f4d891121ad3f83aeac79f536fdc40bce6151a0
mikeqiu2011 <mikeqiu2011@163.com>
2022-06-30 20:30:21 +0800

    rotate and save pdf

--- pdf/pdf.py
+++ pdf/pdf.py
@@ -2,4 +2,11 @@ import PyPDF2
 
 with open('dummy.pdf', 'rb') as file:
     reader = PyPDF2.PdfFileReader(file)
-    print(reader.numPages)
+    page = reader.getPage(0)
+    print(page)  # get page obj
+    page.rotate(180)
+    writer = PyPDF2.PdfFileWriter()
+    writer.addPage(page)
+    with open('tilt.pdf', 'wb') as file_out:
+        writer.write(file_out)
+
