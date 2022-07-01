9c330c525d229aa4176db7afcba00cc615d2895f
mikeqiu2011 <mikeqiu2011@163.com>
2022-07-01 18:37:13 +0800

    use subtext and pprint

--- scrape/scape.py
+++ scrape/scape.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 url = 'https://news.ycombinator.com/news'
 res = requests.get(url)
@@ -8,26 +9,29 @@ soup = BeautifulSoup(res.text, 'html.parser')
 # print(soup.body.contents)
 
 links = soup.select('.titlelink')
-votes = soup.select('.score')
+# votes = soup.select('.score') # some links have no score, but all have subtext
+subtexts = soup.select('.subtext')
 
 
-def create_custom_hackernews(links, votes):
+def create_custom_hackernews(links, subtexts):
     hn = []
     for idx, item in enumerate(links):
         # title = links[idx].getText()
         title = item.getText()
         href = item.get('href', None)
-        points = int(votes[idx].getText().split(' ')[0])
-        if points >= 100:
-            hn.append({
-                'title': title,
-                'link': href,
-                'points': points
-            })
+        vote = subtexts[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().split(' ')[0])
+            if points >= 100:
+                hn.append({
+                    'title': title,
+                    'link': href,
+                    'points': points
+                })
 
     return hn
 
 
-hacker_news = create_custom_hackernews(links, votes)
+hacker_news = create_custom_hackernews(links, subtexts)
 
-print(hacker_news)
+pprint.pprint(hacker_news)
