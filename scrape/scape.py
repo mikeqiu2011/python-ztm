521d774433758fab1e4976d13d35b680d74a6b90
mikeqiu2011 <mikeqiu2011@163.com>
2022-07-01 17:54:32 +0800

    soup basics

--- scrape/scape.py
+++ scrape/scape.py
@@ -3,5 +3,8 @@ from bs4 import BeautifulSoup
 
 url = 'https://news.ycombinator.com/news'
 res = requests.get(url)
-print(res)
-print(res.text)
+
+soup = BeautifulSoup(res.text, 'html.parser')
+# print(soup.body.contents)
+
+print(soup.find_all('div'))  # get all the div tags
