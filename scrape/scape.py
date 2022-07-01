0cd02ff792f11dacdbe2895b64fd54a361301c2c
mikeqiu2011 <mikeqiu2011@163.com>
2022-07-01 18:25:52 +0800

    get title, link and score

--- scrape/scape.py
+++ scrape/scape.py
@@ -10,4 +10,24 @@ soup = BeautifulSoup(res.text, 'html.parser')
 links = soup.select('.titlelink')
 votes = soup.select('.score')
 
-print(votes)
+
+def create_custom_hackernews(links, votes):
+    hn = []
+    for idx, item in enumerate(links):
+        # title = links[idx].getText()
+        title = item.getText()
+        href = item.get('href', None)
+        points = int(votes[idx].getText().split(' ')[0])
+        if points >= 100:
+            hn.append({
+                'title': title,
+                'link': href,
+                'points': points
+            })
+
+    return hn
+
+
+hacker_news = create_custom_hackernews(links, votes)
+
+print(hacker_news)
