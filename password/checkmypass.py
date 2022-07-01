481127e1e76ba55ec09e9f6aa9ed159291e829fa
mikeqiu2011 <mikeqiu2011@163.com>
2022-07-01 14:24:26 +0800

    sha1

--- password/checkmypass.py
+++ password/checkmypass.py
@@ -1,8 +1,26 @@
 import requests
+import hashlib
+
 
 # https://passwordsgenerator.net/sha1-hash-generator/
 
-url = 'https://api.pwnedpasswords.com/range/' + 'CBFDA'
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    resp = requests.get(url)
+    if resp.status_code != 200:
+        raise RuntimeError(f'Error fetching: {resp.status_code}, check the '
+                           f'api and try again')
 
-resp = requests.get(url)
-print(resp)
+    return resp
+
+
+def pwned_api_check(password):
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    print(sha1password)
+    
+    return sha1password
+
+
+query_char = '12345'
+pwned_api_check(query_char)
+# request_api_data(query_char)
