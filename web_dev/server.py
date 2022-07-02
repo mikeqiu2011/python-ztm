4a36022bf7c07321142b226b0d7689a02323e8be
mikeqiu2011 <mikeqiu2011@163.com>
2022-07-02 08:36:51 +0800

    url_for('static', filename='favicon.ico') and template engine

--- web_dev/server.py
+++ web_dev/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -10,6 +10,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+    print(url_for('static', filename='favicon.ico'))  # /static/favicon.ico
     return render_template('index.html')
 
 
