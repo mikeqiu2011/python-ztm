f2670ab44be8175b0e38f4cdf7ce4ca94c79309b
mikeqiu2011 <mikeqiu2011@163.com>
2022-07-02 14:22:20 +0800

    dynamic routing according to path parameter

--- web_dev/server.py
+++ web_dev/server.py
@@ -13,21 +13,6 @@ def hello_my_home():
     return render_template('index.html')
 
 
-@app.route('/works.html')
-def work():
-    return render_template('works.html')
-
-
-@app.route("/about.html")
-def about():
-    return render_template('about.html')
-
-
-@app.route("/contact.html")
-def contact():
-    return render_template('contact.html')
-
-
-@app.route("/components.html")
-def components():
-    return render_template('components.html')
+@app.route('/<string: page_name>')
+def html_page(page_name=None):
+    return render_template(page_name)
